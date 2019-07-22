e713bbb1c171a790251f856e4f4aa60887b25415
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-07-22 13:33:58 +0100

    changed all magic numbers to constants

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,17 @@
 function Thermostat() {
-  this.temperature = 20;
+  this.DEFAULT_TEMP = 20;
+  this.temperature = this.DEFAULT_TEMP;
   this.powerSave = true;
+  this.MIN_TEMP = 10;
+  this.POWER_SAVE_MAX_TEMP = 25;
+  this.MAX_TEMP = 32;
+  this.MID_ENERGY_USAGE = 18;
 };
 
 Thermostat.prototype.up = function() {
-  if (this.temperature === 25 && this.powerSave === true) {
+  if (this.temperature === this.POWER_SAVE_MAX_TEMP && this.powerSave === true) {
     throw "Power saving mode on!";
-  } else if (this.temperature === 32 && this.powerSave === false) {
+  } else if (this.temperature === this.MAX_TEMP && this.powerSave === false) {
     throw "Too hot!"
   }
   else {
@@ -15,7 +20,7 @@ Thermostat.prototype.up = function() {
 };
 
 Thermostat.prototype.down = function() {
-  if (this.temperature === 10) {
+  if (this.temperature === this.MIN_TEMP) {
     throw "Too cold!";
   } else {
     return this.temperature = this.temperature -1;
@@ -23,13 +28,13 @@ Thermostat.prototype.down = function() {
 };
 
 Thermostat.prototype.reset = function() {
-  return this.temperature = 20;
+  return this.temperature = this.DEFAULT_TEMP;
 };
 
 Thermostat.prototype.usage = function() {
-  if (this.temperature < 18) {
+  if (this.temperature < this.MID_ENERGY_USAGE) {
     return "low-usage";
-  } else if (this.temperature < 25) {
+  } else if (this.temperature < this.POWER_SAVE_MAX_TEMP) {
     return "medium-usage";
   } else
     return "high-usage";
