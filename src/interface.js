c57bb9450b86f2874700721d056a3bbe5c854a0b
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-09-05 13:20:42 +0100

    added reset and usage functions to jquery interface

--- src/interface.js
+++ src/interface.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
 
     $('#off').text(thermostat.powerSaveOff());
 
+    $('usage').text(thermostat.usage());
+
+    $('reset').on('mouseup', function() {
+      thermostat.reset();
+      $('temperature').text(thermostat.temperature);
+    });
+
     $('#up').on('mouseup', function() {
       thermostat.up();
       $('#temperature').text(thermostat.temperature);
