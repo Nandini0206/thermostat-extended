ad80c5f233d489c2931e01384762387f08291221
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-09-05 13:03:04 +0100

    added up/down function to jquery interface

--- src/interface.js
+++ src/interface.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
 	  var thermostat = new Thermostat();
 	  $('#temperature').text(thermostat.temperature);
-})
 
-$(document).ready(function() {
-	  var thermostat = new Thermostat();
     $('#on').text(thermostat.powerSaveOn());
-})
 
-$(document).ready(function() {
-  var thermostat = new Thermostat();
-  $('#off').text(thermostat.powerSaveOff());
+    $('#off').text(thermostat.powerSaveOff());
+
+    $('#up').on('mouseup', function() {
+      thermostat.up();
+      $('#temperature').text(thermostat.temperature);
+    });
+
+    $('down').on('mouseup', function(){
+      thermostat.down();
+      $('#temperature').text(thermostat.temperature);
+    });
 })
